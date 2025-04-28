6ce2df5ef0c929f7b67549f986be4ae07e8d85bf
Richard Hsin <hsinr@rose-hulman.edu>
2025-04-27 22:19:49 -0400

    added maven project and dependency

--- minitorrent/src/main/java/com/minitorrent/TorrentMaker.java
+++ minitorrent/src/main/java/com/minitorrent/TorrentMaker.java
@@ -1,5 +1,19 @@
 package com.minitorrent;
 
+import com.github.cdefgah.bencoder4j.*;
+import com.github.cdefgah.bencoder4j.model.BencodedByteSequence;
+import com.github.cdefgah.bencoder4j.model.BencodedDictionary;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 public class TorrentMaker {
-    
+    public void createTorrent(TorrentMetadata metadata, String outputFileName, String announceURL) throws IOException {
+        BencodedDictionary torrentDict = new BencodedDictionary();
+        String trackerURL = announceURL;
+        torrentDict.put(new BencodedByteSequence("announce"), new BencodedByteSequence(trackerURL));
+    }
 }
