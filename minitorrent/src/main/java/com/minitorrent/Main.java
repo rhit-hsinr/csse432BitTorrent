d5e62bc18f00181b87b70f51f223aa96c6e1dc5b
Richard Hsin <hsinr@rose-hulman.edu>
2025-05-01 18:04:17 -0400

    torrentmaker working

--- minitorrent/src/main/java/com/minitorrent/Main.java
+++ minitorrent/src/main/java/com/minitorrent/Main.java
@@ -1,7 +1,26 @@
 package com.minitorrent;
 
+import com.github.cdefgah.bencoder4j.CircularReferenceException;
+
+import java.io.File;
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        File inputFile = new File("shrek.png"); 
+        File outputFile = new File("shrek.torrent");
+
+        String announceURL = "http://tracker.opentrackr.org:1337/announce";
+
+        try {
+            TorrentMetadata metadata = TorrentMaker.createMetadata(inputFile, announceURL);
+            TorrentMaker maker = new TorrentMaker();
+            maker.createTorrent(metadata, outputFile.getAbsolutePath());
+            System.out.println("Torrent created: " + outputFile.getAbsolutePath());
+        } catch (IOException | NoSuchAlgorithmException | CircularReferenceException e) {
+            System.err.println("Error creating torrent: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
