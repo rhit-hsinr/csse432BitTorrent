5bf15c358546fa44b8f4819efb79f0ffc5235543
rhit-wilhelk <wilhelk@rose-hulman.edu>
2025-05-19 18:34:08 -0400

    added comment to new file

--- minitorrent/src/main/java/com/minitorrent/soItDoesntGetDeleted.java
+++ minitorrent/src/main/java/com/minitorrent/soItDoesntGetDeleted.java
@@ -2,6 +2,7 @@ package com.minitorrent;
 
 public class soItDoesntGetDeleted {
 
+    // this goes at bottom of bitclient
     public static void sendMsg(Peer peer, torrentMsg msg) {
         byte[] packedMsg = msg.turnIntoBytes();
         peer.write(packedMsg);
